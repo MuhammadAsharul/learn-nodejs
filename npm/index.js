804b25d91b7375a3238bf9d1147840085fe7d8c5
Muhammad Asharul <masharul51@gmail.com>
2024-01-04 09:59:25 +0700

    done npm

---
+++ npm/index.js
@@ -0,0 +1,4 @@
+import { writeToFile } from "./write.js";
+console.info("Hello World");
+
+writeToFile("hello.log", "Muhammad Asharul Maali");
---
+++ npm/lodash.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const name = "MUHAMMAD ASHARUL MAALI";
+const result = _.capitalize(name);
+
+console.info(result);
